d9a4b746d994f3b89855e61f7e49c2f52bd8e882
FareedJaved <fareedjaved@gmail.com>
2024-10-07 23:23:21 -0500

    Working on Leap exercise. Got dividing a year by 4 down

--- leap/src/leap.cob
+++ leap/src/leap.cob
@@ -3,9 +3,18 @@
        ENVIRONMENT DIVISION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
+       01 WS-RESULT PIC 9.
+       01 WS-YEAR PIC 9(4).
+       01 WS-FOUR PIC 9 VALUE 4.
+       01 WS-DIVISIBLE-BY-FOUR PIC 9.
+       01 WS-DIV-RES PIC 9 VALUE ZEROES.
        PROCEDURE DIVISION.
        LEAP.
       * Enter solution here
+      * TODO: If WS-YEAR is divisible by 4 then see if it's divisible by
+      * 100
+       DIVIDE WS-YEAR BY WS-FOUR 
+        GIVING WS-DIV-RES REMAINDER WS-DIVISIBLE-BY-FOUR.
          CONTINUE.
        LEAP-EXIT.
          EXIT.
